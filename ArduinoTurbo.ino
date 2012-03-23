406a252538c3ea47ff220a35f4fc79ac4bcfb250
Emerson Moretto <emerson@moretto.eng.br>
2012-03-23 18:07:45 -0300

    Mapa de injecao (hard-coded) e ajuste fino no algoritmo
    Ta funcionando com o bico injetor! Falta regular o mapa de injecao pelo
    android agora

--- ArduinoTurbo.ino
+++ ArduinoTurbo.ino
@@ -1,7 +1,7 @@
 boolean LCD = false; 
 
 // PIN
-int injectorPin = 13;
+int injectorPin = 8;
 int turboPin = A0; 
 int xPin = A8; 
 int yPin = A9; 
@@ -20,7 +20,12 @@ int updatePause=0;
 int window=30;
 
 int TURBO_THRESHOLD = 1;
-int LAMBDA_THRESHOLD = 10;
+int LAMBDA_THRESHOLD = 9;
+
+
+//
+
+
 
 void setup() {
   
@@ -35,82 +40,97 @@ void setup() {
 
 void fuelInject(int percent){
   
- 
-  		int MAX_CYCLE = 5;
-  		int MIN_CYCLE_WIDTH = 4; // quanto menor, mais ele vai abrir e fechar
-		
-		float prop = round(window * (percent/100));		
-		
-		// se for muito pequeno a proporcao
-		//if(MIN_CYCLE_WIDTH > prop)
-			//MIN_CYCLE_WIDTH = (int) prop;
-		
-		float mod = 0;
-		float div = 0;
-		int i = MAX_CYCLE;
-		
-		for(; i >= 1 ; i--){
-			
-			mod = (int) prop % i;
-			div = prop / i;
-			
-			if(mod == 0 && div >= MIN_CYCLE_WIDTH){
-				break;
-			}
-		}		
-		
-		if(i==0) i++;
-		
-		//adjust do fit the ONLY cycle to half of window
-		float gap = 0;
-		if(i==1)
-			gap = (window - prop) / (i+1);
-		else
-			gap = (window - prop) / (i);
-		
-		float qtdGaps = (window - prop) / gap;
-
-                  char foo1[100];
-
-                  sprintf(foo1,"Janela: %i ms com  %i ",window,percent);
-                  Serial.println(foo1);
+        if(percent == 0){
+          delay(window);
+          return;
+        }
   
-                  char foo2[100];
-                  sprintf(foo2,"%i ciclo(s) de %f ms com intervalos de %f",i,div,gap);
-                  Serial.println(foo2);
+  	int MAX_CYCLE = 5;
+  	int MIN_CYCLE_WIDTH = 4; // quanto menor, mais ele vai abrir e fechar
+	
+	float prop = ( (float) window * ( (float) percent/100));		
+		
+	// se for muito pequeno a proporcao
+	//if(MIN_CYCLE_WIDTH > prop)
+	    //MIN_CYCLE_WIDTH = (int) prop;
+		
+	float mod = 0;
+	float div = 0;
+	int i = MAX_CYCLE;
+		
+	for(; i >= 1 ; i--){
+		
+		mod = (int) prop % i;
+		div = prop / i;
+			
+		if(mod == 0 && div >= MIN_CYCLE_WIDTH){
+			break;
+		}
+	}		
+		
+	if(i==0) i++;
+		
+	//adjust do fit the ONLY cycle to half of window
+	float gap = 0;
+	if(i==1)
+	    gap = (window - prop) / (i+1);
+	else
+	    gap = (window - prop) / (i);
+		
+	float qtdGaps = (window - prop) / gap;
+
+        char foo1[100];
+
+        sprintf(foo1,"Janela: %i ms com  %i percent, Tempo de injecao %i ms ",window,percent, round(prop));
+        Serial.println(foo1);
+  
+        char foo2[100];
+        sprintf(foo2,"%i ciclo(s) de %i ms com intervalos de %i",i, (int) div, (int) gap);
+        Serial.println(foo2);
   
                 // System.out.print("Janela: "+window + "ms com "+percent+"%. Tempo de injecao: "+prop+"ms ==> " );
                 // System.out.println(i +" ciclo(s) de "+div+"ms com intervalos de "+gap+" iniciando em "+gap+"ms");
 				
-		int soma = 0;
-		for(int j=0; j < qtdGaps+i ; j++){
-			if(j % 2 == 0){
-				soma+=gap;
-                                delay(gap);
-				Serial.print("sleep ");
-                                Serial.println(gap);
-			}else{			
-				soma+=div;
-                                digitalWrite(injectorPin,HIGH);
-                                delay(div);
-                                digitalWrite(injectorPin,LOW);
-				Serial.print("FUEL por ");
-                                Serial.println(div);
-			}
+	int soma = 0;
+	for(int j=0; j < qtdGaps+i ; j++){
+		if(j % 2 == 0){
+			soma+=gap;
+                        delay(gap);                        
+			Serial.print("sleep ");
+                        Serial.println(gap);
+		}else{			
+	        	soma+=div;
+                        digitalWrite(injectorPin,HIGH);
+                        delay(div);
+                        digitalWrite(injectorPin,LOW);
+		        Serial.print("FUEL por ");
+                        Serial.println(div);
 		}
-		Serial.println("Total "+soma);
+	}
+	Serial.print("soma:");
+        Serial.println(soma);
 
-                //algo deu MUITO errado... sleepando durante a janela
-                if(soma == 0){
-                  delay(window);
-                }
+        //algo deu MUITO errado... sleepando durante a janela
+        if(soma == 0){
+          Serial.println("nao foi possivel calcular a injecao");
+          delay(window);
+        }
 }
 
 
 void loop() {
   
+  //Lambda MAPA
+  //TODO fazer a regulagem do mapa via Android
+              //0 , 1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12
+  int mapa[] = {90, 80, 70, 60, 50, 40, 40, 30, 20, 20, 20, 0,  0};
+  
+  
+  digitalWrite(injectorPin,LOW);
   i++;
-  delay(2);
+  delay(500);
+  
+  lambda = 122;
   
   //if( i % 8 == 0 ){
    // each 40ms we will read sensors (Turbo and G-Force)
@@ -119,6 +139,8 @@ void loop() {
       Serial.print("Chegando do BT: ");
       Serial.println(Serial1.read());
     }
+    
+    
     
      /**
      * Lambda Sensor
@@ -132,8 +154,21 @@ void loop() {
     Serial1.write(lambda);
     Serial1.write(">");
 
-    Serial.print("l:");
-    Serial.println(lambda);
+   // Serial.print("l:");
+   // Serial.println(lambda);
+    
+    
+    /**
+    DEV
+    */
+    if(Serial.available() > 0){
+      Serial.println("chegando na serial");
+      lambda = Serial.read(); 
+      lambda-=48;     
+    }
+    
+    
+    
     
     /**
     * G-Force
@@ -144,32 +179,28 @@ void loop() {
      * Turbo Pressure
      */
     turbo = analogRead(turboPin);  
-    Serial.println(turbo);
+   // Serial.println(turbo);
     turbo -= 310; // starting with 1.9v and I don't no why
     turbo = map(turbo,0,1024,0,25);    
-    Serial.println((int)turbo);
+    //Serial.println((int)turbo);
   
     Serial1.write("<t");
     Serial1.write((int)turbo);
     Serial1.write(">");
 
-   
-  
+     
     /**
     * Fuel Inject
     */
-    if(turbo >= TURBO_THRESHOLD && lambda <= LAMBDA_THRESHOLD){
+   // Serial.println(lambda);
+    if(lambda <= LAMBDA_THRESHOLD){
+    //if(turbo >= TURBO_THRESHOLD && lambda <= LAMBDA_THRESHOLD){
 
       // atuando entre 20% e 90%
-      int percent = map(lambda, 0, LAMBDA_THRESHOLD, 2, 9);
-      // desinvertendo o percentual (sonda lambda eh ao contrario)
-      percent = 10 - percent;
-      // passando pra 0-100
-      percent *= 100;
-      Serial.print("Injecting...");
-      Serial.println(percent);
-      // injetandoo
-      fuelInject(percent);
+      //int percent = map(lambda, 0, LAMBDA_THRESHOLD, 2, 9);
+      
+      // Usando mapa de injecao
+      fuelInject(mapa[lambda]);
 
     }else{  /// Nao preciso injetar nada!!!
     
