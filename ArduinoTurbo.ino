bb13cafcf609585c34de2969c2bb52e7c84f3880
Emerson Moretto <emoretto@Mac-Pro-de-Emerson.local>
2013-04-03 01:18:31 -0300

    Big scope change! part 1
    refs issue #5

--- ArduinoTurbo.ino
+++ ArduinoTurbo.ino
@@ -1,14 +1,15 @@
 #include "FreqPeriodCounter.h"
 
+//////// RPM 
+
 const byte counterPin = 3; 
 const byte counterInterrupt = 1; // = pin 3
 FreqPeriodCounter counter(counterPin, millis, 0);
 
 
 
+/////// PIN Setup
 
-
-// PIN
 int injectorPin1 = 8;
 int injectorPin2 = 9;
 
@@ -19,7 +20,9 @@ int lambdaPin = A10;
 
 // Global vars
 float turbo = 0;
+float turboInKpa = 0;
 int lambda = 0;
+int rpm = 0;
 int x = 0;  // variable to store the value coming from the sensor
 int y = 0;  // variable to store the value coming from the sensor
 
@@ -27,7 +30,7 @@ int oldX, oldY; //to clean previous G-Force position
 
 int i=0;
 int updatePause=0;
-int window=30;
+
 
 //TODO implementar a alteracao disso via Android/BT
 int TURBO_THRESHOLD = 2;
@@ -40,12 +43,12 @@ void setup() {
   pinMode(injectorPin1, OUTPUT);  
   pinMode(injectorPin2, OUTPUT);  
   
-  Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial.begin(9600); // USB Debug setup
+  Serial1.begin(9600); // Bluetooth setup
 
-  attachInterrupt(counterInterrupt, counterISR, CHANGE);
+  attachInterrupt(counterInterrupt, counterISR, CHANGE); // RPM interrupt setup
 
-  delay(2000);
+  delay(2000); 
 }
 
 void counterISR(){
@@ -97,6 +100,7 @@ Recebe o percentual a ser injetado e preenche o mapa de injecao fracionado em 1m
 */
 boolean predictInject(int mapa[], int percent){
   
+  int window = 30;
         if(percent == 0) return false;              
         
         for(int k=0; k < 30 ; k++)
@@ -105,7 +109,7 @@ boolean predictInject(int mapa[], int percent){
   	int MAX_CYCLE = 6;
   	int MIN_CYCLE_WIDTH = 2; // quanto menor, mais ele vai abrir e fechar
 	
-	float prop = ( (float) window * ( (float) percent/100));		
+	float prop = ( (float) 30 * ( (float) percent/100));		
 		
 	// se for muito pequeno a proporcao
 	//if(MIN_CYCLE_WIDTH > prop)
@@ -130,7 +134,7 @@ boolean predictInject(int mapa[], int percent){
 	//adjust do fit the ONLY cycle to half of window
 	float gap = 0;
 	if(i==1)
-	    gap = (window - prop) / (i+1);
+	    gap = ( - prop) / (i+1);
 	else
 	    gap = (window - prop) / (i);
 		
@@ -203,10 +207,19 @@ boolean predictInject(int mapa[], int percent){
 
   //Lambda MAPA
               //0 , 1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12
-  int mapa[] = {80, 70, 60, 50, 40, 30, 20, 20, 10, 0, 0, 0, 0};
+  int mapa1[] = {80, 70, 60, 50, 40, 30, 20, 20, 10, 0, 0, 0, 0};
   int mapa2[] = {60, 60, 50, 40, 40, 30, 20, 10, 0, 0, 0, 0, 0};  
+
+const int MAX_PSI = 15;
+const int MAX_RPM = 7;
+const int RPM_PRECISION = 2; // multiply factor: MAX_RPM x RPM_PRECISION
+
+int mapa[MAX_RPM * 2][MAX_PSI];  
+  
   
 void loop() {
+  
+  
   
   digitalWrite(injectorPin1,LOW);
   digitalWrite(injectorPin2,LOW);
@@ -227,79 +240,18 @@ void loop() {
   7000 233hz 233
   */
   if(counter.ready()){
-    //Serial.print(counter.hertz());
-    //Serial.println(" hz");
+    rpm = counter.hertz();
     Serial1.write("<r");
-    Serial1.write( map(counter.hertz(), 0, 233, 0, 140));
+    Serial1.write( map(rpm, 0, 233, 0, 140) );
     Serial1.write(">");
+    
+    rpm = map(rpm, 0, 233, 0, 7);
   }
   
     /**
     * Recebendo novo Mapa via BT
     */
-    if(Serial1.available() > 0){
-      
-      char c = Serial1.read();
-      Serial.print("Chegando do BT: ");
-      Serial.println(c);
-      
-      // Mapa do bico 1
-      if(c == 'm'){
-       Serial.println("Chegando mapa do bico 1");
-        int buff[] = {0,0,0};
-        int j=0,k=0;
-      
-        while(Serial1.available() > 0){
-          int c = Serial1.read();        
-          
-          if(c == 'n'){
-            Serial.println("mapa 2");
-            break;
-          }
-        
-          if(c == ','){
-            // ler e converter os valores, [0]*10+[1]
-            int item = (buff[0] - 48) * 10;
-            mapa[k] = item;  
-            k++;
-            Serial.println(item);            
-            j=0;          
-          }else{
-            buff[j] = c;
-            j++;
-          }    
-          delay(2);    
-        }
-        Serial.print("Fim mapa do bico 1:");
-        Serial.println(k);
-      }
-      
-      // Mapa do bico 2
-      if(c == 'n'){
-        Serial.println("Chegando mapa do bico 2");
-        int buff[] = {0,0,0};
-        int j=0,k=0;
-      
-        while(Serial1.available() > 0){
-          int c = Serial1.read();        
-        
-          if(c == ','){
-            // ler e converter os valores, [0]*10+[1]
-            int item = (buff[0] - 48) * 10;
-            mapa2[k] = item;  
-            k++;
-            Serial.println(item);            
-            j=0;          
-          }else{
-            buff[j] = c;
-            j++;
-          }        
-          delay(2);
-        }
-        Serial.print("Fim mapa do bico 2:");
-        Serial.println(k);
-      }
-    }
+    readMap();
     
     
     
@@ -324,8 +276,7 @@ void loop() {
       Serial.println("chegando na serial");
       lambda = Serial.read(); 
       lambda-=48;     
-    }
-    
+    }    
    
     
     /**
@@ -336,50 +287,119 @@ void loop() {
    
     /**
      * Turbo Pressure
-     */
+     kPa value = kg/cm2 value x 98.0665     
+     */     
+     
+      turbo = (float) analogRead(turboPin) * 5.0 / 1024.0;      
+      turboInKpa = abs (( (turbo) - ( 0.04 * 5.0 ) ) / ( 5 * 0.00369 ));
+      
+      // Mostrar la temperatura en Celsius per port serie
+      
+      Serial.print("kPa: ");
+      Serial.println(turboInKpa);
+ 
+
+/* ORIGINAL CODE
     turbo = analogRead(turboPin);  
-   // Serial.println(turbo);
+   
     turbo -= 310; // starting with 1.9v and I don't no why
     turbo = map(turbo,0,1024,0,25);    
-    
+*/
+
     Serial1.write("<t");
     Serial1.write((int)turbo);
     Serial1.write(">");
 
      
+    // USB Debug
+    Serial.print("RPM (x1000): "); Serial.println(counter.hertz());
+    
+    Serial.print("Turbo pressure (PSI): "); Serial.println(turbo);
+    
+    Serial.print("Lambda (0-12): "); Serial.write(lambda);
+    
     /**
     * Fuel Inject
     */
-
+   // mapa[
+    
+    
     /****** DEV ********/
     //delay(20);    
     //if(lambda <= LAMBDA_THRESHOLD){ // DEBUG
     /****** DEV ********/
     
-    // Verificacao de Thresholds para atuar!
-    if(turbo >= TURBO_THRESHOLD && lambda <= LAMBDA_THRESHOLD){
-
-      int mapaDiscretoBico1[window];
-      int mapaDiscretoBico2[window]; 
-       
-      // Aqui fazemos a predicao da injecao que preenche o mapa de injecao em valores discretos fragmentados a cada 1 ms
-      // ou seja, mandamos o percentual a ser injetado e ele preenche o mapa assim: 0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1
-      // onde 0=bico fechado e 1=bico aberto
-      predictInject(mapaDiscretoBico1, mapa[lambda]); // Bico 1
-      predictInject(mapaDiscretoBico2, mapa2[lambda]); // Bico 2
-      
-      // Atuando
-      fuelInject(mapaDiscretoBico1, mapaDiscretoBico2);
-      
-      delay(3);
-            
-    }else{  /// Nao preciso injetar nada!!!
     
-      delay(window); // dormindo...    
-    }  
+    
+  
+    
+    
+    
     
 }
 
+void readMap(){
+ 
+  // Map format (ignore spaces):
+  // m 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 ; 1,2,3,4,5,6...
+  
+  
+  if(Serial1.available() > 0){
+      
+      char c = Serial1.read();
+      Serial.print("Bluetooth incoming: ");
+      Serial.println(c);
+      
+      // Receiving map
+      if(c == 'm'){
+        
+        Serial.println("Chegando mapa");
+        int buff[] = {0,0,0};
+        int j=0,k=0;
+        int x=0,y=0;
+        int sum = 0;
+        
+        while(Serial1.available() > 0){
+          int c = Serial1.read();        
+          
+          if(c == 'n'){
+            Serial.println("mapa 2");
+            break;
+          }
+        
+          if(c == ','){
+            // ler e converter os valores, [0]*10+[1]
+            int item = (buff[0] - 48) * 10;
+            mapa[k][x] = item;  
+            k++;            
+            sum += item;            
+            j=0;    
+          }else if(c == ';'){ // new line
+            x++;    
+            k=0;  
+          }else{
+            buff[j] = c;
+            j++;
+          }    
+          delay(2); // Little pause for next data
+        }
+        
+        // DEBUG - printing received map
+        for( int i=0; i < MAX_RPM * 2 ; i++){
+          for( int j=0; i < MAX_PSI ; j++){
+            Serial.print(mapa[i][j]);
+            Serial.print(" ");
+          }
+          Serial.println(" ");
+        }
+        
+        Serial.println(" ");
+        Serial.print("New map saved! Checksum: ");
+        Serial.println(sum);       
+        
+      }
+    }  
+}
 
 void readGForce(){
 
